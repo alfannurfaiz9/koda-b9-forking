4e4a744ca78ab7b7cdb87e6771373bdcc26da4df
rcontrisha <rcontrisha@gmail.com>
2026-07-24 15:01:48 +0700

    feat: add function to determine which character is first unique

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ import randomizer from "./src/randomNumber.js";
 
 console.log("Hello World");
 console.log(`Random Number for Today: ${randomizer(1, 50)}`);
+
+/**
+ * Ridho Contrisha
+ */
+import { firstUnique } from "./src/firstUnique.js";
+
+console.log(firstUnique('aabbcddee'));
+console.log(firstUnique('aabbccdd'));
+console.log(firstUnique('javascript'));
