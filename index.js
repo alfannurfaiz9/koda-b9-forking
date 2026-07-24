29bd972be8975a307d390d36f0908f1a17e03927
carlos <carlosnainggolan@gmail.com>
2026-07-24 14:59:49 +0700

    feat: jewels and stones

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ import randomizer from "./src/randomNumber.js";
 
 console.log("Hello World");
 console.log(`Random Number for Today: ${randomizer(1, 50)}`);
+
+
+/**
+ * Carlos Nainggolan
+ * 
+ */
+
+import jewels from "./src/logicCarlos.js"
+
+console.log("=================================================================")
+console.log(`Jewels: ${jewels(["a", "A"], ["A", "A", "A"])}`)
+console.log("=================================================================")
